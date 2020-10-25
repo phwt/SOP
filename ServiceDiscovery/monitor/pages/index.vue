7f27f1c4b8e4d6d0110cb2268688e94a8c578709
phwt.smwt <phwt.smwt@gmail.com>
2020-10-26 00:02:57 +0700

    Get data from eureka API

--- ServiceDiscovery/monitor/pages/index.vue
+++ ServiceDiscovery/monitor/pages/index.vue
@@ -34,6 +34,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import StatusCard from '@/components/StatusCard'
 
 export default {
@@ -41,103 +42,14 @@ export default {
   components: { StatusCard },
   data() {
     return {
-      application: [
-        {
-          name: 'STUDENTSERVICE',
-          instance: [
-            {
-              instanceId: '192.168.1.10:studentservice:8081',
-              hostName: '192.168.1.10',
-              app: 'STUDENTSERVICE',
-              ipAddr: '192.168.1.10',
-              status: 'UP',
-              overriddenStatus: 'UNKNOWN',
-              port: {
-                $: 8081,
-                '@enabled': 'true',
-              },
-              securePort: {
-                $: 443,
-                '@enabled': 'false',
-              },
-              countryId: 1,
-              dataCenterInfo: {
-                '@class':
-                  'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-                name: 'MyOwn',
-              },
-              leaseInfo: {
-                renewalIntervalInSecs: 30,
-                durationInSecs: 90,
-                registrationTimestamp: 1603643246051,
-                lastRenewalTimestamp: 1603643516048,
-                evictionTimestamp: 0,
-                serviceUpTimestamp: 1603643245537,
-              },
-              metadata: {
-                'management.port': '8081',
-              },
-              homePageUrl: 'http://192.168.1.10:8081/',
-              statusPageUrl: 'http://192.168.1.10:8081/actuator/info',
-              healthCheckUrl: 'http://192.168.1.10:8081/actuator/health',
-              vipAddress: 'studentservice',
-              secureVipAddress: 'studentservice',
-              isCoordinatingDiscoveryServer: 'false',
-              lastUpdatedTimestamp: '1603643246051',
-              lastDirtyTimestamp: '1603643245484',
-              actionType: 'ADDED',
-            },
-          ],
-        },
-        {
-          name: 'TEACHERSERVICE',
-          instance: [
-            {
-              instanceId: '192.168.1.10:teacherservice:8082',
-              hostName: '192.168.1.10',
-              app: 'TEACHERSERVICE',
-              ipAddr: '192.168.1.10',
-              status: 'UP',
-              overriddenStatus: 'UNKNOWN',
-              port: {
-                $: 8082,
-                '@enabled': 'true',
-              },
-              securePort: {
-                $: 443,
-                '@enabled': 'false',
-              },
-              countryId: 1,
-              dataCenterInfo: {
-                '@class':
-                  'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-                name: 'MyOwn',
-              },
-              leaseInfo: {
-                renewalIntervalInSecs: 30,
-                durationInSecs: 90,
-                registrationTimestamp: 1603643242453,
-                lastRenewalTimestamp: 1603643512288,
-                evictionTimestamp: 0,
-                serviceUpTimestamp: 1603643241834,
-              },
-              metadata: {
-                'management.port': '8082',
-              },
-              homePageUrl: 'http://192.168.1.10:8082/',
-              statusPageUrl: 'http://192.168.1.10:8082/actuator/info',
-              healthCheckUrl: 'http://192.168.1.10:8082/actuator/health',
-              vipAddress: 'teacherservice',
-              secureVipAddress: 'teacherservice',
-              isCoordinatingDiscoveryServer: 'false',
-              lastUpdatedTimestamp: '1603643242453',
-              lastDirtyTimestamp: '1603643241726',
-              actionType: 'ADDED',
-            },
-          ],
-        },
-      ],
+      application: [],
     }
+  },
+  async created() {
+    const { data } = await axios.get('http://localhost:8761/eureka/apps/', {
+      Accept: 'application/json',
+    })
+    this.application = data.applications.application
   },
 }
 </script>
